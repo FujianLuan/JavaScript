9683cd5cfabbb49a1a31997714af77d471a6b25d
luanfujian <1043912811@qq.com>
2017-02-13 17:08:53 +0800

    Add files via upload

---
+++ bodyHeightReset.js
@@ -0,0 +1,15 @@
+(function (doc, win) {
+    var docEl = doc.documentElement,
+        resizeEvt = 'orientationchange' in window ? 'orientationchange' : 'resize',
+        recalc = function () {
+            var clientHeight = docEl.clientHeight;
+            if (!clientHeight) return;
+            if(clientHeight>$('body').height()){
+                $('body').height(clientHeight);
+            }
+        };
+
+    if (!doc.addEventListener) return;
+    win.addEventListener(resizeEvt, recalc, false);
+    doc.addEventListener('DOMContentLoaded', recalc, false);
+})(document, window);
